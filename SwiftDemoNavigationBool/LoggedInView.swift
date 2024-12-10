cb510dcef7a4fd027a00d3f7f558fded1c10beac
Milan Dima <milan.dima@vives.be>
2024-12-10 11:36:33 +0100

    added textfield and button disabled

--- SwiftDemoNavigationBool/LoggedInView.swift
+++ SwiftDemoNavigationBool/LoggedInView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct LoggedInView: View {
     @Binding var isLoggedIn: Bool
+   
     var body: some View {
         Text("Hello, you are Logged in!")
         
--- SwiftDemoNavigationBool/LoggedOutView.swift
+++ SwiftDemoNavigationBool/LoggedOutView.swift
@@ -9,13 +9,14 @@ import SwiftUI
 
 struct LoggedOutView: View {
     @Binding var isLoggedIn: Bool
+    @State var name: String = ""
     var body: some View {
         Text("Hello, You are not Logged in!")
-        
+        TextField("name?", text: $name)
         //Simple button with text and action
         Button("Log in") {
             isLoggedIn.toggle()
-        }
+        }.disabled(name.count < 3)
     }
 }
 
